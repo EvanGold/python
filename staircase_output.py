a1bc41aba332896e3ca804fd20fb59bf8b34ec40
root <evangold.eg@gmail.com>
2017-01-22 18:02:07 -0500

    bugfix after entering invalid number

--- staircase_output.py
+++ staircase_output.py
@@ -1,3 +1,4 @@
+#
 import sys
 
 def StairCase(n):
@@ -6,22 +7,25 @@ def StairCase(n):
         try:
             mynum = input('Enter a number between 1 and 100: ')
         except:
-            print ("Must enter an integer only!")
-    spaces=n-1
+            print ("Enter an integer only!")
+
+    spaces=mynum-1
     while (spaces >= 0):
         for x in xrange(0,spaces):
             sys.stdout.write(' ')
-        for w in xrange(0,(n-spaces)):
+        for w in xrange(0,(mynum-spaces)):
             sys.stdout.write('#')
         spaces -= 1
         print
     return
+
+# MAIN
 
 while True:
  try:
       mynum0=input('First enter a number between 1 and 100: ')
       StairCase(mynum0)
  except:
-      print ("First Must enter an integer only!")
+      print ("Enter an integer only!")
 
 sys.exit(0)
